ed8555ef50107604eeb0ec2c969836368d07c2a1
Brendan McCray <rbmccr@gmail.com>
2018-10-12 10:35:51 -0500

    finished advanced challenge, have question on modifying object within an array

--- chap2-objects/script.js
+++ chap2-objects/script.js
@@ -53,7 +53,7 @@ let campaign = {
     crimeAndEnforcement: "approves of annual purge",
   },
   donationFormURL: "https://givesangeryourmoney.net",
-  calendar: ["Thursday", "Friday", "Saturday"],
+  calendar: [{readToSchoolKids: "Dec 25, 2018"}],
   volunteerInfo: {
     name: "Elizabeth Sanger",
     address: "123 I Don't Care Lane",
@@ -77,3 +77,26 @@ let campaign = {
 }
 
 console.log(campaign);
+
+//Advanced challenge
+
+// This function accepts a key and image URL as arguments and defines
+// a new property on the campaign object. Note syntax on Object line
+// IMPORTANT: object properties defined with this method are immutable unless modified (see comments below)
+function addToImageGallery(key, newImage) {
+  let gallery = campaign.images; //reference to images object within campaign object
+  Object.defineProperty(gallery, key, {value: newImage, configurable: true, enumerable: true});
+  // 'configurable: true' allows 1. "delete campaign.images.key" to function properly, or 2. for campaign.images.key to be overwritten. 
+  // 'enumerable: true' allows defined property to show up during enumeration (count) of properties on object
+  // 'writable: true' allows an assignment operator to overwrite the property - not used here because string is a simple URL that does not need to be added/subtracted/etc
+}
+
+function changeBio(string) {
+  campaign.biography = string;
+}
+
+function scheduleEvent(event, eventDate) {
+  let schedule = campaign.calendar;
+  schedule.unshift(Object.defineProperty(schedule, event, {value: eventDate, configurable: true, enumerable: true}));
+  delete schedule[0];
+}
